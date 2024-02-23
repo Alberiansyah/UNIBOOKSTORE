12095097c85078c95dbab8b37d8b6dc35211272c
Alberiansyah <alberiansyah1@gmail.com>
2024-02-24 00:49:47 +0700

    Push

--- data.sql
+++ data.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 23, 2024 at 05:38 PM
+-- Generation Time: Feb 23, 2024 at 06:49 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
